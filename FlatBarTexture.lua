91f5c3b9b699c1dc4b711560c55c791944a90bb5
Casey Raethke <casey.j.raethke@gmail.com>
2021-07-08 11:07:28 -0500

    Initial commit

---
+++ FlatBarTexture.lua
@@ -0,0 +1,3 @@
+local LibSharedMedia = LibStub:GetLibrary("LibSharedMedia-3.0")
+
+LibSharedMedia:Register(LibSharedMedia.MediaType.STATUSBAR, "Flat", [[Interface\AddOns\FlatBarTexture\images\flat_bar_texture.blp]])
